4a84ae1e7788e151677e35cbcdd8e04740feb0d0
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-08 15:25:40 -0300

    Initial commit

---
+++ src/main/java/br/com/zup/checkoutestendidoproposta/CheckoutEstendidoPropostaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.checkoutestendidoproposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutEstendidoPropostaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckoutEstendidoPropostaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/zup/checkoutestendidoproposta/CheckoutEstendidoPropostaApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.zup.checkoutestendidoproposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutEstendidoPropostaApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
